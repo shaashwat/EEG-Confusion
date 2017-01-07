8ca72c7e8b6a643d8ab8cd49ebf894128300a046
shaashwat <shaash317@gmail.com>
2017-01-06 21:18:51 -0500

    reversed previous commit and used a new way of organizing the data into X values (inputs) and Y (output)

--- ConfusionDetector.py
+++ ConfusionDetector.py
@@ -2,22 +2,9 @@ import pandas as pd
 from sklearn import svm
 import numpy as np
 
-data = np.array(pd.read_csv("EEG data.csv", delimiter=","))
+conf = pd.read_csv("EEG data.csv", delimiter=",")
 
-subjectID = data[0, :]
-videoID = data[1, :]
-attention = data[2, :]
-mediation = data[3, :]
-rawSignal = data[4, :]
-delta = data[5, :]
-theta = data[6, :]
-alphaOne = data[7, :]
-alphaTwo = data[8, :]
-betaOne = data[9, :]
-betaTwo = data[10, :]
-gammaOne = data[11, :]
-gammaTwo = data[12, :]
-expectedConfused = data[13, :]
-confusion = data[14, :]
+X = conf[[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
+Y = conf[[14]]
 
-# print (subjectID)
+
