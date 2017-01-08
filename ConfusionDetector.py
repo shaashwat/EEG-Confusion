7d2cb04cf263cb5468ec59d9d20b0a1d719afc4b
shaashwat <shaash317@gmail.com>
2017-01-07 20:53:46 -0500

    added SVM.SVC algorithm

--- ConfusionDetector.py
+++ ConfusionDetector.py
@@ -7,4 +7,7 @@ conf = pd.read_csv("EEG data.csv", delimiter=",")
 X = conf[[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
 Y = conf[[14]]
 
-
+clf = svm.SVC()
+clf.fit(X, Y)
+prediction = clf.predict([20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120])
+print(prediction)
